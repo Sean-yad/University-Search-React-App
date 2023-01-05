e05b792b9b132f36818413dc3a13f0b67bcda9e9
Sean-yad <94640179+Sean-yad@users.noreply.github.com>
2023-01-05 13:12:02 -0600

    updated nav and logo

--- src/App.js
+++ src/App.js
@@ -2,57 +2,68 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
-
 import Rankings from "./Pages/Rankings";
 import ErrorPage from "./Pages/ErrorPage";
 
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <div className="anchor">
-              <Link to="/" className="navlink">
-                {" "}
-                Home{" "}
-              </Link>
-            </div>
-          </li>
-          <li>
-            <div className="anchor">
-              <Link to="/about" className="navlink">
-                {" "}
-                About{" "}
-              </Link>
-            </div>
-          </li>
+      <div className="nav-container">
+        <nav className="flex-container">
+          <div className="logo-ul">
+            <li>
+              <div className="logo-div">
+                <h1 className="logo">
+                  <Link to="/"> University Search </Link>
+                </h1>
+              </div>
+            </li>
+          </div>
+        </nav>
+        <nav>
+          <ul>
+            <li>
+              <div className="anchor">
+                <Link to="/" className="navlink">
+                  {" "}
+                  Discover{" "}
+                </Link>
+              </div>
+            </li>
+            <li>
+              <div className="anchor">
+                <Link to="/about" className="navlink">
+                  {" "}
+                  Events{" "}
+                </Link>
+              </div>
+            </li>
 
-          <li>
-            <div className="anchor">
-              <Link to="/rankings" className="navlink">
-                {" "}
-                Rankings{" "}
-              </Link>
-            </div>
-          </li>
-        </ul>
-      </nav>
+            <li>
+              <div className="anchor">
+                <Link to="/rankings" className="navlink">
+                  {" "}
+                  Rankings{" "}
+                </Link>
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/rankings" element={<Rankings />} />
-
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </main>
       <footer>
         <div class="wrapper">
           <nav className="footer-nav">
-            <a href="/">Home</a>
-            <a href="/Portfolio">Portfolio</a>
-            <a href="/Resume">Resume</a>
+            <a href="/">Discover</a>
+            <a href="/about">Events</a>
+            <a href="/rankings">Rankings</a>
           </nav>
         </div>
       </footer>
