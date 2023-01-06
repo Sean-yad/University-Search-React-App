65d8b6071784906c7173482b4e5bc5e6fa8dd5a2
Sean-yad <94640179+Sean-yad@users.noreply.github.com>
2023-01-06 13:54:56 -0600

    Revert "updated margin"
    This reverts commit d18b1270013f4b632c45eb50f3604d5e329a6c26.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import UniversitySearch from "../Components/UniversitySearch";
 
+import WesternEuropeList from "../Components/WesternEuropeList";
+
 function Home() {
   return (
     <div className="main-container">
