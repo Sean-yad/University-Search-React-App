4acc824fac7d0bb54f0ae320ee57e12792ca2555
Sean-yad <94640179+Sean-yad@users.noreply.github.com>
2023-01-07 15:42:02 -0600

    added search filter

--- src/Components/UniversitySearch.js
+++ src/Components/UniversitySearch.js
@@ -3,22 +3,27 @@ import "./UniversitySearch.css";
 
 function UniversitySearch() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [searchCountry, setSearchCountry] = useState("");
   const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(
-        `http://universities.hipolabs.com/search?country=&name=${searchTerm}`
+        `http://universities.hipolabs.com/search?${searchCountry}&name=${searchTerm}&limit=30`
       );
       const data = await response.json();
       setSearchResults(data);
     }
 
     fetchData();
-  }, [searchTerm]);
+  }, [searchTerm, searchCountry]);
 
   function handleChange(event) {
     setSearchTerm(event.target.value);
+  }
+
+  function handleCountryChange(event) {
+    setSearchCountry(`country=${event.target.value}`);
   }
 
   return (
@@ -31,27 +36,54 @@ function UniversitySearch() {
           placeholder="Search from over 20000 universities across the globe..."
           className="search-box"
         />
+        <label htmlFor="country">Country: </label>
+        <select
+          id="country"
+          onChange={handleCountryChange}
+          className="custom-select"
+        >
+          <option value="">All</option>
+          <option value="United States">United States</option>
+          <option value="Canada">Canada</option>
+          <option value="Mexico">Mexico</option>
+          <option value="France">France</option>
+          <option value="Germany">Germany</option>
+          <option value="Italy">Italy</option>
+          <option value="United Kingdom">United Kingdom</option>
+          <option value="Spain">Spain</option>
+          <option value="Russia">Russia</option>
+          <option value="China">China</option>
+          <option value="Japan">Japan</option>
+          <option value="Finland">Finland</option>
+          <option value="Iran">Iran</option>
+          <option value="Bangladesh">Bangladesh</option>
+          <option value="Turkey">Turkey</option>
+          <option value="Norway">Norway</option>
+          <option value="Poland">Poland</option>
+          <option value="Portugal">Portugal</option>
+          <option value="Romania">Romania</option>
+          <option value="Malaysia">Malaysia</option>
+          <option value="Vietnam">Vietnam</option>
+        </select>
       </form>
       <table className="university-table">
         <thead>
           <tr>
             <th>Name</th>
             <th>Country</th>
-            <th>Alpha 2</th>
             <th>Website</th>
           </tr>
         </thead>
         <tbody>
-          {searchResults.slice(0, 20).map((university) => (
+          {searchResults.slice(0, 30).map((university) => (
             <tr key={university.id}>
               <td>{university.name}</td>
               <td>{university.country}</td>
-              <td>{university.alpha_two_code}</td>
               <ul className="url-list">
-                {university.domains.map((domains) => (
-                  <li key={domains}>
-                    <a href={`http://${domains}`} target="_blank">
-                      {domains}
+                {university.domains.map((domain) => (
+                  <li key={domain}>
+                    <a href={`http://${domain}`} target="_blank">
+                      {domain}
                     </a>
                   </li>
                 ))}
